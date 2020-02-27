f42e9aba5e8d1a1d7753472007fb9a49c4df4b97
Matias Garcia <matiasigarcia91@gmail.com>
2020-02-27 17:48:45 +0100

    Removed middleware examples

--- server.js
+++ server.js
@@ -2,19 +2,18 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
-const middlewares = require("./middlewares");
-
 const teamRouter = require("./team/router");
 const playerRouter = require("./player/router");
 
 app.use(express.json()); //Express own bodyParser
-// app.use(logginMiddleware);
 // app.use(bodyParser.json()) // body-parser package
 
 app.use("/team", teamRouter);
 app.use("/player", playerRouter);
 // app.use("/player", playerRouter);
 
-app.get("/", middlewares.logginMiddleware, (req, res) => {});
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 app.listen(port, () => console.log(`App started in port: ${port}`));
